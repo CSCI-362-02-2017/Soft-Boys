e39872a91d1694950ac5cead97601942b2d6e8e7
Jasmine <jasminedimai@gmail.com>
2017-10-31 09:47:42 -0400

    Sugar test cases

--- sugar-master/tests/test.py
+++ sugar-master/tests/test.py
@@ -5,13 +5,17 @@ gi.require_version('GConf', '2.0')
 from gi.repository import GConf
 from cpsection.aboutme import model
 
+cases = ['', ' ','Jim Bowring', '@', '2']
+
 class TestUserProfile(unittest.TestCase):
-	def test(str):
-		model.set_nick(str)
-		if model.get_nick() == str:
+	#def test(str):
+	for testCase in cases:
+		try:
+			model.set_nick(testCase)
+			#if model.get_nick() == " ":
 			print('Pass')
-		else:
+		except ValueError:
 			print('Fail')
-	eval(sys.argv[1])
+		#eval(sys.argv[1])
 	
 	
