df76d46356507ce735385e1abda1064b42775f02
Jasmine <jasminedimai@gmail.com>
2017-11-13 20:25:42 -0500

    Update project files

--- TestAutomation/project/arithmetic-master/puzzles/multiplication.py
+++
@@ -1,9 +0,0 @@
-name = "x"
-sort_key = 50
-
-def get_problem(self, difficulty):
-    x = self.generate_number(difficulty)
-    y = self.generate_number(difficulty)
-    question = "%s x %s" % (x, y)
-    answer = x * y
-    return question, answer
--- TestAutomation/project/arithmetic-master/puzzles/subtraction.py
+++
@@ -1,9 +0,0 @@
-name = "-"
-sort_key = 25
-
-def get_problem(self, difficulty):
-    x = self.generate_number(difficulty)
-    y = self.generate_number(difficulty)
-    question = "%s - %s" % (x, y)
-    answer = x - y
-    return question, answer
